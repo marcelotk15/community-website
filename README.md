4ba1dbe5a8278ad9c376f177b101eb50f517f0b6
Cody Bennett <hi@codyb.co>
2019-11-29 00:22:05 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,24 @@
+# Death's Shadow Community Website
+The website of the Death's Shadow Discord strategy server.
+
+[![Site preview](/public/social-image.png)](https://mtg.codyb.co)
+
+## Install & run
+
+Make sure you have npm and yarn installed. Install dependencies with:
+
+```bash
+yarn
+```
+
+Once it's done start up a local server with:
+
+```bash
+yarn start
+```
+
+To run tests:
+
+```bash
+yarn test
+```
---
+++ src/app/reducer.js
@@ -0,0 +1,21 @@
+import { theme } from 'app/theme';
+
+export const initialState = {
+  currentTheme: theme.dark,
+};
+
+export function reducer(state, action) {
+  switch (action.type) {
+    case 'setTheme':
+      return { ...state, currentTheme: action.value };
+    case 'updateTheme':
+      return { ...state, currentTheme: { ...theme[state.currentTheme.id], ...action.value } };
+    case 'toggleTheme': {
+      const newThemeKey = state.currentTheme.id === 'dark' ? 'light' : 'dark';
+      window.localStorage.setItem('theme', JSON.stringify(newThemeKey));
+      return { ...state, currentTheme: theme[newThemeKey] };
+    }
+    default:
+      throw new Error();
+  }
+}
